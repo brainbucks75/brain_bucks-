2db5907d1c56fc5bfd21affa8233dc2988777c17
brainbucks75 <brainbucks75@gmail.com>
2026-01-05 22:19:25 +0300

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,14 @@
+export const questions = [
+  {
+    id: 1,
+    text: "سؤال تجريبي رقم 1",
+    answers: ["جواب أ", "جواب ب", "جواب ج"],
+    correct: 0
+  },
+  {
+    id: 2,
+    text: "سؤال تجريبي رقم 2",
+    answers: ["جواب أ", "جواب ب", "جواب ج"],
+    correct: 1
+  }
+];
